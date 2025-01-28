e139ffddc45c91261f314e96fd049dc2e7036f38
Bernie Liu <bernieliu42@gmail.com>
2025-01-28 04:08:15 -0800

    new file

---
+++ src/Forfeit.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main (int args, char** argv) {
+
+    if (args >1) {
+        std::cout << "Usage: Forfeit [script]" << std::endl;
+    }
+}
