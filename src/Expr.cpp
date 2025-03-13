7ec68f539480cf3365cb8127937737f48821ecab
Bernie Liu <bernieliu42@gmail.com>
2025-03-13 03:56:21 -0700

    Edit GenerateAst

---
+++ src/Expr.cpp
@@ -0,0 +1,39 @@
+#include "Token.hpp"
+
+class Expr{
+public:
+    class  Binary : public Expr{
+    public:
+      Expr left;
+      Token operator;
+      Expr right;
+      Binary(Expr left, Token operator, Expr right) {
+        this->left = left;
+        this->operator = operator;
+        this->right = right;
+      }
+    };
+    class  Grouping : public Expr{
+    public:
+      Expr expression;
+      Grouping(Expr expression) {
+        this->expression = expression;
+      }
+    };
+    class  Literal : public Expr{
+    public:
+      Object value;
+      Literal(Object value) {
+        this->value = value;
+      }
+    };
+    class  Unary : public Expr{
+    public:
+      Token operator;
+      Expr right;
+      Unary(Token operator, Expr right) {
+        this->operator = operator;
+        this->right = right;
+      }
+    };
+};
--- tool/GenerateAst.cpp
+++ tool/GenerateAst.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <regex>
 #include <list>
 
 using namespace std;
 void defineAst (string outputDir, string baseName, list<string> types);
+string trim (string& str);
+void defineType(ofstream& writer, string baseName, string className, string fieldList);
+vector<string> split(string&str, const string& del);
 
 int main(int args, char** argv) {
     if (args !=2) {
@@ -12,7 +18,9 @@ int main(int args, char** argv) {
         return 1;
     }
     string outputDir=argv[1];
-   
+    
+    
+
     defineAst(outputDir, "Expr", list<string> {"Binary   : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal  : Object value",
@@ -20,12 +28,70 @@ int main(int args, char** argv) {
 }
 
 void defineAst (string outputDir, string baseName, list<string> types) {
-    string path = outputDir + "/" + baseName +".hpp";
+    string path = outputDir + "/" + baseName +".cpp";
 
     ofstream writer(path);
-    
+    writer << "#include \"Token.hpp\""<<endl<<endl;;
     writer << "class "+ baseName +"{"<<endl;
-    writer <<endl;
+    writer << "public:"<<endl;
+    
+    //AST classes
+    for (string type : types) {
+        vector<string> vec=split(type, ":");
+
+        string className=trim(vec[0]);
+        string fields=trim(vec[1]);
+        defineType(writer, baseName, className, fields);
+    }
+
     writer << "};"<<endl;
     writer.close();
+}
+
+void defineType(ofstream& writer, string baseName, string className, string fieldList) {
+    writer << "    class  " + className + " : public "+ baseName + "{"<<endl;
+    writer << "    public:"<<endl;
+
+    vector<string> fields=split(fieldList, ", ");
+
+    for (string field : fields) {
+        //string type=split(field, " ")[0];
+        //string name = split(field, " ")[1];
+        writer << "      "+field+";"<<endl;
+    }
+
+    //constructor
+
+    writer << "      " + className+ "("+fieldList+") {"<<endl;
+
+    //Store parameters in fields
+    
+    
+
+    for (string field : fields) {
+        string name = split(field, " ")[1];
+        writer << "        this->"+name+" = "+name+";"<<endl;
+    }
+    writer<<"      }"<<endl;
+    writer<<"    };"<<endl;
+    
+    
+
+    
+    
+}
+
+string trim(string& str) {
+    //leading and trailing whitespace
+    regex pattern("^\\s+|\\s+$");
+    return regex_replace(str, pattern, "");
+}
+
+vector<string> split(string&str, const string& del) {
+    regex pattern(del);
+    sregex_token_iterator it(str.begin(), str.end(), pattern, -1);
+    sregex_token_iterator end;
+    vector<std::string> result(it, end);
+
+    return result;
 }
